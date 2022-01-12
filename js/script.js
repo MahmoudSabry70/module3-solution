509be764d2136cc9c7a82d5f6f08d663e3f68052
Mahmoud Sabry <96152155+MahmoudSabry70@users.noreply.github.com>
2022-01-12 21:05:20 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function()
-{
-    $("#button_menuicon").blur(function (event){
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 576){
-            $("#collapseExample").collapse('hide');
-        }
-    });
-});
